bd318f4c0d78c8a7d8d9fc00d80be0a5526c459e
stephenpaul2727 <stephenpaul2727@gmail.com>
2017-12-04 12:22:40 -0500

    Added carousel captions

--- index.php
+++ index.php
@@ -33,15 +33,31 @@ session_start();
          <div class="carousel-inner">
             <div class="carousel-item active">
                <img class="d-block w-100" src="static/images/slider-1.png" alt="First slide">
+               <div class="carousel-caption d-none d-md-block">
+                  <h1>Welcome to Mother bear's Pizza</h1>
+                  <p>The best in Bloomington</p>
+                </div>
             </div>
             <div class="carousel-item">
                <img class="d-block w-100" src="static/images/slider-2.png" alt="Second slide">
+               <div class="carousel-caption d-none d-md-block">
+                  <h1>The Illinois Central (Medium)</h1>
+                  <p><a class="btn btn-primary" href="#"><i class="fa fa-plus"></i>&nbsp;Order Now</a></p>
+                </div>
             </div>
             <div class="carousel-item">
                <img class="d-block w-100" src="static/images/slider-3.png" alt="Third slide">
+               <div class="carousel-caption d-none d-md-block">
+                  <h1>The Summer Garden (Medium)</h1>
+                  <p><a class="btn btn-primary" href="#"><i class="fa fa-plus"></i>&nbsp;Order Now</a></p>
+                </div>
             </div>
             <div class="carousel-item">
                <img class="d-block w-100" src="static/images/slider-4.png" alt="Fourth slide">
+               <div class="carousel-caption d-none d-md-block">
+                  <h1>Veggie Delight (Small)</h1>
+                  <p><a class="btn btn-primary" href="#"><i class="fa fa-plus"></i>&nbsp;Order Now</a></p>
+                </div>
             </div>
          </div>
          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
