c57de8337fb0d2d4b61b22b88b4883eecd9443ff
anchalkhandelwal <ankhande@iu.edu>
2017-12-06 12:26:26 -0500

    menu basic structure

--- menu.php
+++ menu.php
@@ -1,43 +1,63 @@
+<?php
+session_start();
+?>
 <!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-	<script src="script.js" type="text/javascript"></script>
-	<title>Menu - Mother Bear's Pizza</title>
-	<script>
+<html lang="en">
+   <head>
+      <title>MotherBears | Home</title>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  	  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	  <script src="script.js" type="text/javascript"></script>
+      <!-- PAGE CSS -->
+      <link rel="stylesheet" href="static/css/bootstrap_custom.css"/>
+      <link rel="stylesheet" type="text/css" href="static/css/commons.css">
+	   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	   <link rel="stylesheet" type="text/css" href="static/css/style.css">
+	   
+	   	<script>
   $( function() {
     $( "#tabs" ).tabs();
   } );
   </script>
-	<?php include('connection.php'); ?>
-</head>
-<body>
-	<div id="main-container">
-		<div id ="header-container"><?php include('header.php'); ?></div>
+   </head>
+	
+<body class="dom-body">
+		<?php include('header.php'); ?>
+
+		<?php
+			 if(isset($_SESSION['first_name'])){
+				echo '<div class="alert alert-primary" role="alert">Welcome back, '.$_SESSION["first_name"].'!<a class="btn btn-danger btn-sm float-right" href="signout.php" style="position:relative;top:-3px;">Logout</a></div>';
+			 }
+		?>
+	<div class="content-container">
 		<div id="tabs">
 		  <ul>
-			<li><a href="#tabs-1">Nunc tincidunt</a></li>
-			<li><a href="#tabs-2">Proin dolor</a></li>
+			<li><a href="#tabs-1">View All</a></li>
+			<li><a href="#tabs-2">Pizza</a></li>
 			<li><a href="#tabs-3">Aenean lacinia</a></li>
 		  </ul>
 		  <div id="tabs-1">
-			<p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+			<p> <?php include('view_all_menu.php'); ?></p>
 		  </div>
 		  <div id="tabs-2">
-			<p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+			<p><?php 
+				$category_id = 1;
+				include('pizza_menu.php'); ?></p>
 		  </div>
 		  <div id="tabs-3">
 			<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
 			<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
 		  </div>
-		</div>
-		<div id ="footer-container"><?php include('footer.php'); ?></div>
+			
+	</div>		
 	</div>
-	
+		<?php include('footer.php'); ?>
 	
 </body>
 </html>
--- pizza_menu.php
+++ pizza_menu.php
@@ -1,11 +1,24 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php 
+include 'connection.php';
 
-<body>
-	<p>testing 1 2 3</p>
-</body>
-</html>
+$query = 'SELECT * FROM "Menu" WHERE category_id = '.$category_id ; 
+
+$rs = pg_query($conn, $query) or die("Cannot execute query: $query\n");
+	echo "<div class='menu_all'>";
+while ($row = pg_fetch_row($rs)) {
+	unset($item_name, $item_description, $cost_small, $cost_medium, $cost_large, $item_photo);
+	$item_name = $row[2];
+	$item_description = $row[3];
+	$cost_small = $row[5];
+	$cost_medium = $row[4];
+	$cost_large = $row[6];
+	$item_photo = $row[7];
+	echo "<div class='category_section'>";
+	echo "<img class='categoryicons img-fluid' src='static/images/item/$item_photo' alt='pizza'>";
+	
+	echo "<span class='category_name'>$item_name"."</span>";
+	echo "</div>";
+}
+echo "</div>";
+pg_close($con); 
+?>
